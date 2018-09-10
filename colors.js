84f15aa2800423bf14c2bb1197594b83986bc60f
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-09-10 23:23:12 +0200

    Add js code

---
+++ colors.js
@@ -0,0 +1,5 @@
+const btnOrange = document.querySelector('.orange');
+const btnGreen = document.querySelector('.green');
+const btnBlue = document.querySelector('.blue');
+const btnYellow = document.querySelector('.yellow');
+const btnRed = document.querySelector('red');
