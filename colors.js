278ade1dc862abee0b11d2b87c9dfa39137d7c96
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-09-10 23:31:14 +0200

    Update js code

--- colors.js
+++ colors.js
@@ -2,4 +2,16 @@ const btnOrange = document.querySelector('.orange');
 const btnGreen = document.querySelector('.green');
 const btnBlue = document.querySelector('.blue');
 const btnYellow = document.querySelector('.yellow');
-const btnRed = document.querySelector('red');
+const btnRed = document.querySelector('.red');
+
+function changeColor() {
+    // const selectedColor = this.className;
+    // document.body.className = selectedColor;
+    document.body.className = this.className;
+}
+
+btnOrange.addEventListener('click', changeColor);
+btnGreen.addEventListener('click', changeColor);
+btnBlue.addEventListener('click', changeColor);
+btnYellow.addEventListener('click', changeColor);
+btnRed.addEventListener('click', changeColor);
